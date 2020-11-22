d3a565d806c64ef3e52ade8a5208f2123ca5e713
IanCode09 <ianlombu@protonmail.com>
2020-11-22 13:41:19 +0700

    Edit Icon Helper

--- App.js
+++ App.js
@@ -28,11 +28,11 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs') 
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs') 
 
 // Handlebars
 app.engine('.hbs', exphbs({ 
-  helpers: { formatDate, stripTags, truncate }, 
+  helpers: { formatDate, stripTags, truncate, editIcon }, 
   defaultLayout: 'main', 
   extname: '.hbs' 
 }))
@@ -49,6 +49,12 @@ app.use(session({
 // Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set Global User
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
