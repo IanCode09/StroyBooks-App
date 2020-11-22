bfd036ed6eae327bd965a0365d0be6aded187a13
IanCode09 <ianlombu@protonmail.com>
2020-11-22 15:11:29 +0700

    User Stories

--- routes/stories.js
+++ routes/stories.js
@@ -127,5 +127,25 @@ router.delete('/:id', ensureAuth, async(req, res) => {
     }
 })
 
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async(req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+
+        res.render('stories/index', {
+            stories
+        })
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500')
+    }
+})
+
 
 module.exports = router
