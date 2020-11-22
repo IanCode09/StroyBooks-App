b004d4b928d98f8b72087b87a65f097541642f0d
IanCode09 <ianlombu@protonmail.com>
2020-11-22 10:54:53 +0700

    Dashboard Stories

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,24 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login',
     })
 })
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async(req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+        })
+    } catch (error) {
+        console.log(error)
+        res.render('error/500')
+    }
 })
 
 
