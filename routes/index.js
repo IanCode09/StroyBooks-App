1403210ef18ef43e99562d4ba9810bdd8e513d4c
IanCode09 <ianlombu@protonmail.com>
2020-11-22 08:19:05 +0700

    Auth Middleware

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login',
     })
 })
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
+    console.log(req.user)
     res.render('dashboard')
 })
 
